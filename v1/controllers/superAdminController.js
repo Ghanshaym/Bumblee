7399796ea1dcc26c6beebd6bc1f2bb0b4df941dd
unknown <poonergagan@gmail.com>
2020-12-06 17:01:53 +0530

    add changes

--- v1/controllers/superAdminController.js
+++ v1/controllers/superAdminController.js
@@ -1103,7 +1103,9 @@ async function dashBoard(req, res) {
         let  earningByAdmin = [] , admin , pendingLoan = 0 ,LoanAmount =0 , expances = 0
         console.log("totalAdminDetail",totalAdminDetail);
         console.log("totalAdminDetail",totalAdminDetail.length);
-
+        totalAdminDetail.map((demo)=>{
+            console.log("demo",demo);
+        })
         if(totalAdminDetail.length>0){
             totalAdminDetail.map((obj)=>{
                 earningByAdmin.push({email:obj.email,borrower:obj.Borrower.length})
