82704efc5faf47522c8ad29617391d4af202718f
unknown <poonergagan@gmail.com>
2020-12-23 23:13:47 +0530

    add changes

--- index.js
+++ index.js
@@ -19,10 +19,33 @@ app.use('/api', route);
 app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 app.use(cors())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+app.use('*', function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept,authtoken"
+    );
     next();
   });
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+  })
+  
 app.use('/static', express.static(path.join(__dirname+'/uploads')));
 
 app.use(express.static((path.join(__dirname,"../server/swagger"))));
